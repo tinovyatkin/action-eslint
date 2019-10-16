eaffeb51055b2fc010b3f4b10807c6990f2d5f7e
Konstantin Vyatkin <tino@vtkn.io>
2019-10-16 10:39:58 -0400

    add tsx and jsx to extensions

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,8 @@
 export const CHECK_NAME = 'ESLint Results';
-export const EXTENSIONS_TO_LINT = new Set(['.mjs', '.js', '.ts']);
+export const EXTENSIONS_TO_LINT = new Set([
+  '.mjs',
+  '.js',
+  '.ts',
+  '.jsx',
+  '.tsx'
+]);
